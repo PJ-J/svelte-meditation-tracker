5a825366b771216975b3486d408e799eff5f6b37
PJ Jones <pj.jones4000.com>
2022-07-05 13:29:37 -0500

    working on auth

---
+++ src/components/Auth.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { supabase } from "../supabase"
+
+  let loading = false;
+  let email;
+</script>
+
+<h1 class="text=2xl font-bold text-center text-gray-800 md:text-3xl">Log In</h1>
+<p class="text-center mt-2">Sign in via magic link with your email below.</p>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,11 @@
 <script>
-  import "../app.css"
+  import { supabase } from "../supabase";
+  import Auth from "../components/Auth.svelte";
+  import "../app.css";
+  
 </script>
 
 <div class ="container mx-auto my-6 max-w-lg">
+  <Auth/>
   <slot></slot>
 </div>
