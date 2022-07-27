2444cd2147c06f0c9c5cbc8b8865842ea32d89d1
PJ Jones <pj.jones4000.com>
2022-07-27 11:15:57 -0500

    accordion entry list

--- src/components/EntryForm.svelte
+++ src/components/EntryForm.svelte
@@ -12,7 +12,7 @@
 <form class="my-6" on:submit|preventDefault={handleSubmit}>
 	<div class="flex flex-col mb-2">
 		<label class="text-lg mb-2" for="entry">How many minutes did you meditate?</label>
-		<input type="number" bind:value={entry} name="minutes" placeholder="minutes" class="focus:font-black appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"/>
+		<input type="number" bind:value={entry} name="minutes" placeholder="minutes" class="text-black appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"/>
 	</div>
   <button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Submit</button>
 </form>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,46 @@
 <script>
-	import EntryForm from '../components/entryForm.svelte';
+	import EntryForm from '../components/EntryForm.svelte';
 	import Entry from '../components/Entry.svelte';
 	import index from '../components/Entry.svelte';
 	import { entries } from '../stores/entrystore';
 	import format from 'date-fns/format';
 	import compareDesc from 'date-fns/compareDesc';
+	import { slide } from 'svelte/transition'
+	import Chevron from "../components/Chevron.svelte";
+	export let open = true;
+	let onClick = () => {
+    open = !open;
+  };
 
 	// get minutes from each DB entry into own array
-	$: minutesArray = $entries.map(entry => entry.minutes);
+	$: minutesArray = $entries.map((entry) => entry.minutes);
 
 	// sum function to pass into reduce
 	const totalMinutes = (total, minutes) => {
 		return total + minutes;
-	}
+	};
 
 	// reduce minutes array to total minutes
 	$: totalMins = minutesArray.reduce(totalMinutes, 0);
 	// convert to hours w 2 decimal places
-	$: totalHours = ((totalMins / 60).toFixed(2) + " hours")
-  
-  // getting all dates in their own clean array
-	$: dateArray = $entries.map((entry) =>
-		entry.created_at.slice(0, 10).replace(/-/g, ', ')
-	);
-  
-  //function for comparing dates in the dateArray to see if they're desc or ascending
-  const compareFunction = (array) => {
-    let result = [];
-    for (let index = 0; index < array.length; index++) {
-      result.push(compareDesc(new Date(array[index]), new Date(array[index + 1])));
-    }
-    return result;
-  }
-  
-  $: compare = compareFunction(dateArray);
+	$: totalHours = (totalMins / 60).toFixed(2) + ' hours';
 
-  //now going to use the compare array, and if date is ascending(meaning value = 1), then add that to streak
-  $: streak = compare.filter(value => value == 1).length + 1;
+	// getting all dates in their own clean array
+	$: dateArray = $entries.map((entry) => entry.created_at.slice(0, 10).replace(/-/g, ', '));
+
+	//function for comparing dates in the dateArray to see if they're desc or ascending
+	const compareFunction = (array) => {
+		let result = [];
+		for (let index = 0; index < array.length; index++) {
+			result.push(compareDesc(new Date(array[index]), new Date(array[index + 1])));
+		}
+		return result;
+	};
+
+	$: compare = compareFunction(dateArray);
+
+	//now going to use the compare array, and if date is ascending(meaning value = 1), then add that to streak
+	$: streak = compare.filter((value) => value == 1).length + 1;
 
 	// $: compareArray = returnDate.filter((date))
 
@@ -59,7 +63,16 @@
 
 	<p>{totalSessions} entries</p>
 	<p>{hour} long sessions</p>
-	{#each $entries as entry}
-		<Entry {entry} index={entry.id} />
-	{/each}
+	<section>
+		<header class="flex bg-gray-500 rounded">
+			<Chevron {open} {onClick} /> Entry List
+		</header>
+		{#if open}
+		<div transition:slide={{ duration: 500}}>
+			{#each $entries as entry}
+				<Entry {entry} index={entry.id} />
+			{/each}
+		</div>
+		{/if}
+	</section>
 </main>
